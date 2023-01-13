c5b314018c749d4b71b98c2bd59ef3f485eaaef2
Steve Auxter <116984055+SteveAuxter@users.noreply.github.com>
2023-01-12 20:27:58 -0500

    Update search_help.php

--- search_help.php
+++ search_help.php
@@ -17,13 +17,13 @@
         
         <ul>
             <li><h3>Search: Find & Sort</h3></li>
-            <p><a href="https://github.com/SteveAuxter/CBOD/wiki/Search" target="_blank">CBOD wiki on GitHub</a></p>
             <p>Enter full or partial Asset ID, Serial Number, or Note - then press <b>Search</b>.</p>
             <p>(Hint) Leave the search field blank - then press <b>Search</b> - will show all devices.</p>
             <p>Sorting will always default to Asset ID, but can be toggled to Serial Number or Notes field.</p>
             <p>Clicking linked Asset ID will open Asset ID > Device Info, showing details direct from Google query - <i>including</i> Recent User List.</p>
             <p>Clicking linked Serial Number will open Serial Number > Device Info, showing details direct from Google query - <i>including</i> Recent User List.</p>
             <p>Clicking linked Device ID will open the device page directly in Google Admin console.</p>
+            <p><a href="https://github.com/SteveAuxter/CBOD/wiki/Search" target="_blank">Check the wiki on GitHub</a></p>
         </ul>
 
         <?php include "footer.php" ?>
